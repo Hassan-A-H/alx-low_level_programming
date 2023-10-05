54688af3f22d2e55dd49f5cb0ab18d9517ba554e
Hassan Ahmed <h23798a@gmail.com>
2023-10-05 13:54:32 +0000

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program, just print text to the console
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
