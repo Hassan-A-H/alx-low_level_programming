4c5fbda8bb27c9478974c0f5b948b9a4fd476c99
Hassan Ahmed <h23798a@gmail.com>
2023-10-10 23:28:34 +0000

    a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+
+/**
+ * multiple_3_5 - Calculates the sum of multiples of 3 or 5 below a given number
+ *
+ * @n: The upper limit for finding multiples
+ *
+ * Description:
+ * This function calculates the sum of all multiples of 3 or 5 that are less
+ * than the given number 'n'. It uses a loop to iterate through numbers from 0
+ * to 'n - 1' and adds the multiples to a running sum. The final sum is printed.
+ */
+
 void multiple_3_5(int);
 
 int main(void)
