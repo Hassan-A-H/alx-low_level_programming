816f4d21184d358dcda5751dbf57610f29fac5d7
Hassan Ahmed <h23798a@gmail.com>
2023-10-13 00:49:49 +0000

    edit style

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,11 +2,37 @@
 
 void largest_prime_factor(unsigned long n);
 
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ * The 'main' function serves as the entry point for the program.
+ * It calls the 'largest_prime_factor' function to find and print the largest
+ * prime factor of that number.
+ *
+ * Return:
+ * Returns 0 upon successful execution.
+ */
+
 int main(void)
 {
 	largest_prime_factor(612852475143);
 	return (0);
 }
+
+/**
+ * largest_prime_factor - Find and print the largest prime factor of a number.
+ * @n: The number for which to find the largest prime factor.
+ *
+ * Description:
+ * This function calculates and prints the largest prime factor of the given
+ * number 'n'. It employs a loop to iteratively divide 'n' by progressively
+ * larger prime factors, and it keeps track of the largest prime factor found.
+ * After completing the factorization, it prints the largest prime factor.
+ *
+ * Return:
+ * This function does not return a value (void).
+ */
 
 void largest_prime_factor(unsigned long n)
 {
@@ -19,7 +45,7 @@ void largest_prime_factor(unsigned long n)
 		n /= 2;
 	}
 
-	for(i = 3; i <= sqrt(n); i += 2)
+	for (i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
 		{
