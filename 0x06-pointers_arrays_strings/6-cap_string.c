5b1fe024328cb21f4468313e4479c32c438bad99
Hassan <h23798a@gmail.com>
2023-10-18 22:36:35 +0300

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,7 @@ char *cap_string(char *str)
 		    str[i] == '{' ||
 		    str[i] == '}')
 		{
-			if (str[i + 1] >= 'a' && str[i + 1])
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 			continue;
 		}
