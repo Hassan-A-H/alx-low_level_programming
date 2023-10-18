c77c0a9cd80a030c94e31033fec3d92603b3ce40
Hassan <h23798a@gmail.com>
2023-10-18 22:56:05 +0300

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,22 +12,22 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 || str[i] == ' ' ||
-		    str[i] == '\t' ||
-		    str[i] == '\n' ||
-		    str[i] == ',' ||
-		    str[i] == ';' ||
-		    str[i] == '.' ||
-		    str[i] == '!' ||
-		    str[i] == '?' ||
-		    str[i] == '"' ||
-		    str[i] == ')' ||
-		    str[i] == '(' ||
-		    str[i] == '{' ||
-		    str[i] == '}')
+		if (i == 0 || str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}')
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] -= 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
 			continue;
 		}
 	}
