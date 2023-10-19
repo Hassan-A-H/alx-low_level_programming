2a03f77b1a27aca081245c1c6318a32b0b801f00
Hassan <h23798a@gmail.com>
2023-10-19 18:08:25 +0300

    a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -28,14 +28,12 @@ void print_buffer(char *b, int size)
 	{
 		if (i % 10 == 0)
 			printf("%08x: ", i);
-		for (j = i; j < i + 9; j += 2)
+		for (j = i; j < i + 10; j += 2)
 		{
 			if ((j < size) && ((j + 1) < size))
 				printf("%02x%02x: ", b[j], b[j + 1]);
 			else
 			{
-				while (++j <= i + 10)
-					printf(" ");
 				printf(" ");
 			}
 		}
