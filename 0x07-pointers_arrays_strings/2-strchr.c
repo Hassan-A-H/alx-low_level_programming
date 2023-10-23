50e1c30408abc41c7f58a5ac7216d8de154a470e
Hassan <h23798a@gmail.com>
2023-10-23 19:44:45 +0300

    edit logic

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -17,11 +17,12 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (*s == c)
+			return (s);
 		i++;
+		s++;
 	}
 	return (NULL);
 }
