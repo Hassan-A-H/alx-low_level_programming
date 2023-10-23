5f5d5f6187fdef6f8d50a7357ca8a03300cd7b6e
Hassan <h23798a@gmail.com>
2023-10-23 10:54:42 +0300

    a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,11 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stddef.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
