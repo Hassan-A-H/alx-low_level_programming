03a773062fbe7d6808bc1cca2bc67970ede83a8a
Hassan <h23798a@gmail.com>
2023-11-02 11:51:33 +0200

    edit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,18 +10,22 @@
 
 int *array_range(int min, int max)
 {
-	int *array;
+	int *block;
 	int i, j = 0;
 
 	if (min > max)
 		return (NULL);
-
-	array = malloc((max - min) * sizeof(int) + 1);
-	if (array == NULL)
+	block = malloc(sizeof(*block) * ((max - min) + 1));
+	if (block != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			block[j] = i;
+			j++;
+		}
+		return (block);
+	}
+	else
 		return (NULL);
 
-	for (i = min; i <= max; i++)
-		array[j++] = i;
-
-	return (array);
 }
