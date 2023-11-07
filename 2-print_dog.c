a0b0fc39dd87a78fefe4fa7ac4b9943341568516
Hassan <h23798a@gmail.com>
2023-11-07 12:55:31 +0200

    a function that prints a struct dog

---
+++ 2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* print_dog - prints a struct dog
+*@d: dog identification
+*
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
--- dog.h
+++ dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+#include <stdio.h>
+
 /**
  * struct dog - define a new data type
  * represnting a dog
@@ -16,5 +19,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
