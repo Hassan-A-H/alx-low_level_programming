37c2a57ababcd2f18dcf9f63eb89300cf4b70165
Hassan <h23798a@gmail.com>
2023-11-07 12:38:03 +0200

    Define a new type struct dog with the following elements

---
+++ dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a new data type
+ * represnting a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
