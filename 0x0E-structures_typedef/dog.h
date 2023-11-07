b59b68894194df164e34d079053d7d9d4cc4dd46
Hassan <h23798a@gmail.com>
2023-11-07 13:33:14 +0200

    a function that frees dogs.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - define a new data type
@@ -25,5 +26,7 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
