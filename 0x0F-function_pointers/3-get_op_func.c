f57239e3b7ce9ba648a53a6dd2af4d0d1020bb8e
Hassan <h23798a@gmail.com>
2023-11-09 08:03:58 +0200

    a program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
 *get_op_func - selects the correct function to perform
@@ -10,22 +9,21 @@
 *Return: A pointer to the function corresponding to
 *the operator given in parameter
 */
-int (*get_op_func(char *s))(int, int)
+int (*get_op_func(char *s))(int a, int b)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
+	i = 0;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
-
-return (ops[i].f);
-
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
 * main - Prints the result of simple operations.
@@ -9,33 +7,31 @@
 *
 * Return: Always 0.
 */
-
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int num1, num2;
-char *op;
+	int num1, num2;
+	char *operator;
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
 
-num1 = atoi(argv[1]);
-op = argv[2];
-num2 = atoi(argv[3]);
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*operator == '/' || *operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-if (get_op_func(op) == NULL || op[1] != '\0')
-{
-printf("Error\n");
-exit(99);
-}
-
-if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
-{
-printf("Error\n");
-exit(100);
-}
-printf("%d\n", get_op_func(op)(num1, num2));
-return (0);
+	printf("%d\n", get_op_func(operator)(num1, num2));
+	return (0);
 }
