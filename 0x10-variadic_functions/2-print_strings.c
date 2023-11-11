20cd0a64f0f855d440e8661ed55d381cfc15766e
Hassan <h23798a@gmail.com>
2023-11-11 08:02:11 +0200

    edit str == null condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,24 +7,26 @@
 *
 *Return: nothing
 */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+
 	va_list args;
 	unsigned int i;
 	char *str;
 
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
 		if (str == NULL)
-			printf("(nli)");
+			printf("(nil)");
 		else
 			printf("%s", str);
 
-		if (separator != NULL && i != (n - 1))
+		if (i != (n - 1) && separator != NULL && str == NULL)
 			printf("%s", separator);
+
 	}
 
 	printf("\n");
