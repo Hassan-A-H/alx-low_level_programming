d72de1a5db0b7ee9756e3342ea72b3e6f381218c
Hassan <h23798a@gmail.com>
2023-11-11 10:02:21 +0200

    a function that prints anything.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,16 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
