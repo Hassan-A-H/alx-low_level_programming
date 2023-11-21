723f826bf59b78e01b6ab6b77b5b6fc046d19b4d
Hassan <h23798a@gmail.com>
2023-11-21 02:24:25 +0200

    a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,12 +1,9 @@
 #include "lists.h"
 
 /**
- *listint_len - Returns the number of elements
- *in a linked listint_t list
- *@h: A pointer to the head of the list
- *
- *
- *Return: the number of elements in the listint_t list
+ * listint_len - Returns the number of elements in a linked listint_t list
+ * @h: A pointer to the head of the list
+ * Return: the number of elements in the listint_t list
  */
 size_t listint_len(const listint_t *h)
 {
@@ -22,34 +19,34 @@ size_t listint_len(const listint_t *h)
 	return (count);
 }
 
-
 /**
  * insert_nodeint_at_index - returns the nth node of a linked list
  * @head: pointer to the head of the list
  * @idx: index of the node to be added
  * @n: content of the new node
- *
  * Return: the address of the node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *curr = *head;
 	listint_t *new = NULL;
+	size_t len;
 	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL || idx > listint_len(curr))
-	{
-		free(new);
+	len = listint_len(curr);
+
+	if (idx > len)
 		return (NULL);
-	}
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
-
 	if (idx == 0)
 	{
 		new->next = curr;
@@ -69,5 +66,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			i++;
 		}
 	}
+
 	return (new);
 }
