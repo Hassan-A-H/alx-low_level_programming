1ea0fc209b961fce317dfe746feadd77b09e2abc
Hassan <h23798a@gmail.com>
2023-11-21 19:57:22 +0200

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,61 +1,42 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _r - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
- *
- * Return: pointer to the new list
- */
-const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
-{
-	const listint_t **newlist;
-	size_t i;
+* print_listint_safe - Prints a listint_t linked list
+*
+* @head: listint_t head
+*
+* Return: size_t
+*/
 
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
-}
 
-/**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the start of the list
- *
- * Return: the number of nodes in the list
- */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, num = 0;
-	const listint_t **list = NULL;
+	size_t count1 = 0, count2;
+	const listint_t *temp = head;
+	const listint_t *next_node;
 
-	while (head != NULL)
+	while (temp != NULL)
 	{
-		for (i = 0; i < num; i++)
+		printf("[%p] %d\n", (void *)temp, temp->n);
+
+		count1++;
+
+		temp = temp->next;
+		next_node = head;
+
+		count2 = 0;
+
+		while (count2 < count1)
 		{
-			if (head == list[i])
+			if (temp == next_node)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(list);
-				return (num);
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (count1);
 			}
+
+			next_node = next_node->next;
+			count2++;
 		}
-		num++;
-		list = _r(list, num, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
 	}
-	free(list);
-	return (num);
+	return (count1);
 }
