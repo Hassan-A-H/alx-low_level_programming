567ff6fa6efdb162f2f4fe2039fecd2c6a97e633
Hassan <h23798a@gmail.com>
2023-11-21 02:25:19 +0200

    a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,71 +1,49 @@
 #include "lists.h"
 
-/**
- * listint_len - Returns the number of elements in a linked listint_t list
- * @h: A pointer to the head of the list
- * Return: the number of elements in the listint_t list
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *curr = h;
-	size_t count;
-
-	count = 0;
-	while (curr != NULL)
-	{
-		curr = curr->next;
-		count++;
-	}
-	return (count);
-}
 
 /**
- * insert_nodeint_at_index - returns the nth node of a linked list
- * @head: pointer to the head of the list
- * @idx: index of the node to be added
- * @n: content of the new node
- * Return: the address of the node
+ * insert_nodeint_at_index - Inserts a new node at a given position in a
+ * listint_t list.
+ * @head: A pointer to a pointer to the head of the list
+ * @idx: The index where the new node should be added
+ * @n: The data for the new node
+ * Return: The address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *curr = *head;
-	listint_t *new = NULL;
-	size_t len;
-	unsigned int i = 0;
+	listint_t *new_node, *temp;
+	unsigned int i;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 
-	len = listint_len(curr);
-
-	if (idx > len)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+	new_node->n = n;
 
-	new->n = n;
-	new->next = NULL;
 	if (idx == 0)
 	{
-		new->next = curr;
-		*head = new;
-	}
-	else
-	{
-		while (curr != NULL)
-		{
-			if (i == idx - 1)
-			{
-				new->next = curr->next;
-				curr->next = new;
-				break;
-			}
-			curr = curr->next;
-			i++;
-		}
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 
-	return (new);
+	temp = *head;
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+	{
+		temp = temp->next;
+	}
+
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = temp->next;
+	temp->next = new_node;
+
+	return (new_node);
 }
