69857bea972ae11f3e43f256d3d3cb002b14f419
Hassan <h23798a@gmail.com>
2023-11-24 00:46:04 +0200

    a function that sets the value of a bit to 0 at a given index.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include "stddef.h"
 #include "stdio.h"
 
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
