2c93ce837d54e5ea0fcb98c664b891f1db7bd6a2
Hassan <h23798a@gmail.com>
2023-11-24 00:40:38 +0200

    a function that sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include "stddef.h"
 #include "stdio.h"
 
+int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
