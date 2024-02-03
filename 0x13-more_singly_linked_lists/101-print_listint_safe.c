d132af63b7a122e89f172273e06a6836cb60bab7
Hassan <h23798a@gmail.com>
2024-02-03 08:23:00 +0200

    another solution

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,20 @@
 #include "lists.h"
 
 int loop_exist(const listint_t *head);
-
+/**
+ * print_listint_safe - Prints a listint_t linked list safely, detecting loops
+ * @head: Pointer to the head of the listint_t list
+ *
+ * Description: This function prints the elements of a listint_t linked list
+ * safely, handling cases where the list may contain a loop. It uses the
+ * Floyd's Cycle Detection algorithm to detect loops in the list. If a loop
+ * is detected, it prints all the nodes in the list up to the node where
+ * the loop starts, followed by the node at which the loop starts. If no loop
+ * is detected, it prints all the elements of the list. It also prints the
+ * memory addresses of the nodes along with their integer values.
+ *
+ * Return: Number of nodes in the list (excluding nodes in the loop)
+ */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *fast = head;
@@ -17,7 +30,7 @@ size_t print_listint_safe(const listint_t *head)
 			if (slow == fast)
 			{
 				fast = fast->next;
-				while(slow != fast)
+				while (slow != fast)
 				{
 					count1++;
 					fast = fast->next;
@@ -53,11 +66,25 @@ size_t print_listint_safe(const listint_t *head)
 	}
 	return (sum);
 }
-
+/**
+ * loop_exist - Checks if a loop exists in a linked list
+ * @head: Pointer to the head of the listint_t list
+ *
+ * Description: This function checks if a loop exists in a linked list
+ * using Floyd's Cycle Detection algorithm. It iterates through the list
+ * using two pointers, a slow pointer and a fast pointer. The slow pointer
+ * moves one node at a time while the fast pointer moves two nodes at a time.
+ * If the two pointers meet at some node during traversal, it indicates the
+ * presence of a loop in the list. The function returns 1 if a loop is found,
+ * otherwise returns 0.
+ *
+ * Return: 1 if a loop exists, 0 otherwise
+ */
 int loop_exist(const listint_t *head)
 {
 	const listint_t *fast = head;
 	const listint_t *slow = head;
+
 	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
