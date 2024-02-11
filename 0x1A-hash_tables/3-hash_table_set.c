07e86c48029e30fc63152f64332ed8b5e9f0702d
Hassan <h23798a@gmail.com>
2024-02-11 05:08:40 +0200

    another solution

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
 
+hash_node_t *hash_node(const char *key, const char *value);
+void free_node(hash_node_t *node);
+void free_table(hash_table_t *table);
+
 /**
  * hash_table_set - Add or update an element in a hash table.
  * @ht: A pointer to the hash table.
@@ -11,43 +15,94 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
+	hash_node_t *current_node;
+	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+
+	if (ht->array[index])
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(ht->array[index]->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			strcpy(ht->array[index]->value, value);
+			return (1);
+		}
+		else
+		{
+			current_node = hash_node(key, value);
+			if (current_node == NULL)
+			{
+				free_node(current_node);
+				return (0);
+			}
+
+			current_node->next = ht->array[index];
+			ht->array[index] = current_node;
 			return (1);
 		}
 	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	current_node = hash_node(key, value);
+	if (current_node == NULL)
 	{
-		free(value_copy);
+		free_node(current_node);
 		return (0);
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
+	ht->array[index] = current_node;
 	return (1);
+}
+
+/**
+ * hash_node - create new node
+ * @key: The key cannot be an empty string.
+ * @value: The value associated with key.
+ * Return: A pointer to newly created node
+ */
+hash_node_t *hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	new_node->key = malloc(sizeof(strlen(key) + 1));
+	new_node->value = malloc(sizeof(strlen(value) + 1));
+	new_node->next = NULL;
+	strcpy(new_node->key, key);
+	strcpy(new_node->value, value);
+	return (new_node);
+}
+
+/**
+ * free_node - Deallocate node.
+ *
+ * @node: To be freed
+ * Return: Nothing
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+
+/**
+ * free_table - Deallocate table.
+ *
+ * @table: To be freed
+ * Return: Nothing
+ */
+void free_table(hash_table_t *table)
+{
+	hash_node_t *temp;
+	unsigned long int i;
+
+	for (i = 0; i < table->size; i++)
+	{
+		temp = table->array[i];
+		if (temp)
+			free_node(temp);
+	}
+	free(table->array);
+	free(table);
 }
